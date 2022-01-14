06c201d2f300a25ddf43bddfe70369fa42a8cd26
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-01-13 22:29:56 -0500

    test integrations

---
+++ 06_Integration_test/file_0.py
@@ -0,0 +1,30 @@
+from auth import Authentication, Authorization
+import unittest
+
+class TestAuthentication(unittest.TestCase):
+    def test_login(self):
+        auth =Authentication()
+        auth.USERS = [{"username": "testuser", "password": "testpass"}]
+        resp = auth.login("testuser", "testpass")
+        assert resp == {"username": "testuser", "password": "testpass"}
+
+class TestAuthorization(unittest.TestCase):
+    def test_can(self):
+        authz = Authorization()
+        authz.PERMISSIONS = [{"user": "testuser", "permissions": {"create"}}]
+        resp = authz.can({"username": "testuser"}, "create")
+        assert resp is True
+
+class TestAutorizeAthenticatedUser(unittest.TestCase):
+    def test_auth(self):
+        auth = Authentication()
+        authz = Authorization()
+        auth.USERS = [{"username": "testuser", "password": "testpass"}]
+        authz.PERMISSIONS = [{"user": "testuser",
+            "permissions": {"create"}}]
+        u = auth.login("testuser", "testpass")
+        resp = authz.can(u, "create")
+        assert resp is True
+
+if __name__ == '__main__':
+    unittest.main()
